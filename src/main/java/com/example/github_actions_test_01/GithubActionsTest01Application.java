13deaf8feabff58b6ba68295f4e9e1a8ceeddf44
bokyoungp <bokyoungp@gmail.com>
2024-06-14 22:24:00 +0900

    first commit

---
+++ src/main/java/com/example/github_actions_test_01/GithubActionsTest01Application.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_test_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsTest01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsTest01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/github_actions_test_01/GithubActionsTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_test_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsTest01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
